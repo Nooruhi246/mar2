b1ad248bc3f3015d4958e63892fdf459a2439f23
Noorruhi246 <ruhitcse.rymec@gmail.com>
2022-04-06 21:30:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactMarkdown from 'react-markdown';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      markdown: "",
+      loading: true
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.changeLoadingVar = this.changeLoadingVar.bind(this);
+  }
+  handleChange(e){
+    this.setState({
+      markdown: e.target.value
+    })
+  }
+  changeLoadingVar(){
+    this.setState({
+      loading: false
+    })
+  }
+
+  componentDidMount(){
+    setTimeout(this.changeLoadingVar,3000)
+  }
+  render(){
+    if(this.state.loading){
+      return (<div><h1>Loading...</h1></div>)
+    }
+ 
+    return(
+      <div className="app">
+        <textarea onChange={this.handleChange}/>
+        <ReactMarkdown className="preview" children={this.state.markdown}/>
+      </div>
+    )
+  }
 }
-
+ 
 export default App;
